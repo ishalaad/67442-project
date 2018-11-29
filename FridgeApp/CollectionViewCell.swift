31e5d3f3468de4bac630e876140972c4b1738ad0
Isha Laad <ilaad@andrew.cmu.edu>
2018-11-29 16:17:25 -0500

    add common items working and changed UI of scan in page

---
+++ FridgeApp/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  FridgeApp
+//
+//  Created by Isha Laad on 11/29/18.
+//  Copyright © 2018 Sydney Bauer. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+	@IBOutlet weak var myLabel: UILabel!
+	@IBOutlet weak var pic: UIImageView!
+	
+	
+}
